5ee86ea21b13449350267910f2ff039afed4a103
cszdlq <812236044@qq.com>
2018-03-05 15:58:35 +0800

    修改代码格式，补齐括号

--- src/v1ch02/Welcome/Welcome.java
+++ src/v1ch02/Welcome/Welcome.java
@@ -9,10 +9,11 @@ public class Welcome
 {
    public static void main(String[] args)
    {
-      String greeting = "Welcome to Core Java!";
+      String greeting = "Welcome to Core Java 10!";
       System.out.println(greeting);
-      for (int i = 0; i < greeting.length(); i++)
+      for (int i = 0; i < greeting.length(); i++) {
          System.out.print("=");
+      }
       System.out.println();
    }
 }
